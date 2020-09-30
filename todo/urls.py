dbe3786e957f6d541be4f2e87c7be730d4df5a3a
Wiktor90 <w-witkowski90@o2.pl>
2020-09-30 18:24:20 +0200

    Backend

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tasks, name='tasks'),
+    path('', views.tasks_list, name='tasks_list'),
+    path('task_edit/<int:pk>/', views.edit_task, name='edit_task'),
+    path('task_delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
-def tasks(request):
+def tasks_list(request):
     tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {
         'tasks': tasks,
+        'form': form,
     }
     return render(request, 'todo/base.html', context)
+
+def edit_task(request, pk):
+    task = Task.objects.get(pk=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request, 'todo/edit_task.html', {'form': form})
+
+def delete_task (request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+
+    return render(request, 'todo/delete_task.html', {'task': task})
