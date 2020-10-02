6b6caec18cbf8f9798fcc59078a9498f71f89901
Wiktor90 <w-witkowski90@o2.pl>
2020-10-02 15:11:53 +0200

    Add Register + Login page html/css

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.tasks_list, name='tasks_list'),
     path('task_edit/<int:pk>/', views.edit_task, name='edit_task'),
     path('task_delete/<int:pk>/', views.delete_task, name='delete_task'),
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .forms import TaskForm
+from .forms import TaskForm, CreateAccount
+
+
+def registerPage(request):
+    form = CreateAccount()
+
+    if request.method == 'POST':
+        form = CreateAccount(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'todo/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'todo/login.html', context)
+
+def logoutUser(request):
+    context = {}
+    return redirect(request, 'todo/login.html', context)
 
 def tasks_list(request):
     tasks = Task.objects.all()
